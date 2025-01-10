53568d034f8b1c0b2203f219c4a784c8d6110b1b
Alena Radomskaia <radomskaia.a.i@gmail.com>
2025-01-11 01:18:40 +0700

    feat: add modal window

---
+++ simon-says/src/js/modal.js
@@ -0,0 +1,48 @@
+import { createDOMElement } from "@/js/utils.js";
+import { resetGame } from "@/js/resetGame.js";
+import { gameState, newGame } from "@/js/newGame.js";
+
+export function createModal() {
+  const modal = createDOMElement({
+    tagName: "dialog",
+    classList: [
+      "modal",
+      "flex",
+      "flex--column",
+      "flex--align-center",
+      "flex_gap-20",
+    ],
+  });
+  const modalButton = createDOMElement({
+    tagName: "button",
+    classList: ["button", "modal-button"],
+    textContent: "Play again",
+  });
+  const modalText = createDOMElement({
+    tagName: "p",
+  });
+  const modalText2 = createDOMElement({
+    tagName: "p",
+    textContent: "Simon says: ",
+  });
+  const secretWord = createDOMElement({
+    tagName: "strong",
+  });
+  modalText2.append(secretWord);
+
+  modal.append(modalText, modalText2, modalButton);
+  document.body.append(modal);
+
+  modalButton.addEventListener("click", () => {
+    modal.close();
+    newGame();
+  });
+
+  gameState.elements.modal = {
+    modal: modal,
+    text: modalText,
+    word: secretWord,
+  };
+
+  return modal;
+}
